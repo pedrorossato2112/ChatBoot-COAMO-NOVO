4b5b59bfb174b61b9b5f3347f106cd4aea1d72e9
𝒫𝑒𝒹𝓇𝑜 ℛ𝓈𝓈𝒶𝓉𝑜𝑜𝓃 <rossato.pedro@escola.pr.gov.br>
2025-09-03 17:23:28 -0300

    fim por hoje

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const celular = document.getElementById('celular');
 const toggle = document.getElementById('toggleTheme');
-const solIcon = document.getElementById('solIcon');
 
 toggle.addEventListener('click', () => {
-  celular.classList.toggle('dark');
+  celular.classList.toggle('dark'); // alterna o tema
 
   if(celular.classList.contains('dark')){
     toggle.src = 'imagens/brilho-do-sol.png'; // ícone sol no dark mode
   } else {
-    toggle.src = 'imagens/lua.png'; // volta para a lua no modo claro
+    toggle.src = 'imagens/lua.png'; // ícone lua no modo claro
   }
+});
